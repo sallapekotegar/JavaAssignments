abb3dbae64140a43c8734312b55cb29441b017fb
sallapekotegar <99339235+sallapekotegar@users.noreply.github.com>
2022-02-10 00:03:16 +0530

    Add files via upload

--- src/com/Assignment/Assignment1/Program1.java
+++ src/com/Assignment/Assignment1/Program1.java
@@ -1,28 +1,26 @@
-package com.assignment1;
-
+package com.Assignments.Assignment1;
 /* 1. Write a program to calculate the factorial of a number using a while loop.
  *The factorial of a positive number n is given by:
- *factorial of n (n!) = 1 * 2 * 3 * 4 * ... * n 
+ *factorial of n (n!) = 1 * 2 * 3 * 4 * ... * n
  */
+public class Program1 {
 
-public class Assignment1_1 {
+        int f=8;
+        public int factorial(int n) {
+            if (n==0)	return 1;
+            else {
+                while (n>0) {
+                    f=f*n;
+                    n=n-1;
+                }
+                return f;
+            }
+        }
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
 
-	int f=1;
-	public int factorial(int n) {
-		if (n==0)	return 1;
-		else {
-			while (n>0) {
-				f=f*n;
-			    n=n-1;
-			}
-			return f;
-		}
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Assignment1_1 obj = new Assignment1_1();
-		System.out.println(obj.factorial(3));
-	}
+            Program1 obj = new Program1();
+            System.out.println(obj.factorial(3));
+        }
 
 }
